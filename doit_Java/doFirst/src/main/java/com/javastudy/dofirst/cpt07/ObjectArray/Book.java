50e1f9672079061aa0ccb1fc11709d84dbf253f0
[strong1133] <strong1133@naver.com>
2021-08-15 23:23:31 +0900

    객체 배열 공부

---
+++ doit_Java/doFirst/src/main/java/com/javastudy/dofirst/cpt07/ObjectArray/Book.java
@@ -0,0 +1,22 @@
+package com.javastudy.dofirst.cpt07.ObjectArray;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private String bookName;
+    private String author;
+
+    public Book(){}  // 가본 생성자
+
+    public Book(String bookName, String author){
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public
+
+
+}
