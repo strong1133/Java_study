1a5b6b19c664240e815da827cda0d4fd3101c13e
[strong1133] <strong1133@naver.com>
2021-10-09 15:28:40 +0900

    주석작업

--- doit_Java/doFirst/src/main/java/com/javastudy/dofirst/cpt10/interfaceApplication/MyClassTest.java
+++ doit_Java/doFirst/src/main/java/com/javastudy/dofirst/cpt10/interfaceApplication/MyClassTest.java
@@ -4,13 +4,16 @@ public class MyClassTest {
     public static void main(String[] args) {
         MyClass myClass = new MyClass();
 
-        
+        // InterfaceX 으로 대입하면 InterfaceX에서 선언한 메서드만 호출가능
         InterfaceX xclass = myClass;
         xclass.x();
 
         InterfaceY yclass = myClass;
         yclass.y();
 
+        // MyClass형은  MyInterface 구현 하였고
+        // MyInterface는 x, y를 상속 받기 때문에
+        // MyClass형은 MyInterface내에 구현된 모든 메서드에 접근 가능
         myClass.myMethod();
         myClass.x();
         myClass.y();
